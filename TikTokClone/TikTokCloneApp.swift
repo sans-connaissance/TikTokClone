09d6032f21a95de5de331065f82e53f4dbd81faa
sans-connaissance <davemalicke@gmail.com>
2021-09-20 14:39:41 -0400

    Initial Commit

---
+++ TikTokClone/TikTokCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  TikTokCloneApp.swift
+//  TikTokClone
+//
+//  Created by David Malicke on 9/20/21.
+//
+
+import SwiftUI
+
+@main
+struct TikTokCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
