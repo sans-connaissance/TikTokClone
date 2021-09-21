6a0f7237fbde75c8c0e4e243d9cf24fb961dac48
sans-connaissance <davemalicke@gmail.com>
2021-09-21 14:57:19 -0400

    Created PlayerView struct upon a UIKit view in order to get the video player up and running.

--- TikTokClone/View/Home.swift
+++ TikTokClone/View/Home.swift
@@ -6,11 +6,21 @@
 //
 
 import SwiftUI
+import AVKit
 
 struct Home: View {
     
     @State var index = 0
     @State var top = 0
+    
+    @State var data = [
+        Video(id: 0, player: AVPlayer(url: URL(fileURLWithPath: Bundle.main.path(forResource: "video4", ofType: "mp4")!)), replay: false),
+        Video(id: 1, player: AVPlayer(url: URL(fileURLWithPath: Bundle.main.path(forResource: "video5", ofType: "mp4")!)), replay: false),
+        Video(id: 2, player: AVPlayer(url: URL(fileURLWithPath: Bundle.main.path(forResource: "video2", ofType: "mp4")!)), replay: false),
+        Video(id: 3, player: AVPlayer(url: URL(fileURLWithPath: Bundle.main.path(forResource: "video3", ofType: "mp4")!)), replay: false),
+        Video(id: 4, player: AVPlayer(url: URL(fileURLWithPath: Bundle.main.path(forResource: "video1", ofType: "mp4")!)), replay: false),
+        Video(id: 5, player: AVPlayer(url: URL(fileURLWithPath: Bundle.main.path(forResource: "video6", ofType: "mp4")!)), replay: false),
+    ]
     
     var body: some View {
         ZStack {
---
+++ TikTokClone/View/Player.swift
@@ -0,0 +1,50 @@
+//
+//  Player.swift
+//  TikTokClone
+//
+//  Created by David Malicke on 9/21/21.
+//
+
+import SwiftUI
+import AVKit
+
+
+struct PlayerView: View {
+    @Binding var data: [Video]
+    
+    
+    var body: some View {
+        VStack(spacing: 0) {
+            ForEach(0 ..< data.count) { index in
+                ZStack {
+                    Player(player: data[index].player)
+                        .frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height)
+                        .offset(y: -5)
+                }
+            }
+        }
+    }
+}
+
+
+struct Player: UIViewControllerRepresentable {
+    
+    var player: AVPlayer
+   
+    func makeUIViewController(context: Context) -> AVPlayerViewController {
+        
+        let view = AVPlayerViewController()
+        view.player = player
+        view.showsPlaybackControls = false
+        view.videoGravity = .resizeAspectFill
+        
+        return view
+        
+    }
+    
+    
+    func updateUIViewController(_ uiViewController: AVPlayerViewController, context: Context) {
+        
+    }
+    
+}
